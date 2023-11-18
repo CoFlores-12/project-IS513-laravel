3457d47fe3a10efa5d82ba09b53ce21cb7fa7542
josueamador19 <amadoroscar789@gmail.com>
2023-11-18 16:54:59 -0600

    landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/home.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1e8824e8c2.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid containermain">
+        <div class="row align-items-start">
+            <div class="col left">
+            </div>
+            <div class="col right  flex-column " >
+                <div class="container-fluid d-grid gap-2 d-md-flex justify-content-md-center" style="height=50%">
+                    <nav class="navbar navbar-expand-lg navbar-light ">
+                        <div class="container-sm container-md">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                                <li class="nav-item ">
+                                    <a class="nav-link active  text-light " aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link  text-light " href="#">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link  text-light " href="#">Artists</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link  text-light " href="#">Shop</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link  text-light " href="#">Contact</a>
+                                </li>
+                        </div>
+                    </nav>
+                </div>
+                <div class="row " style="height=50%">
+                    <div class="container-sm container-md ">
+                        <p class="text-center fs-1 fw-bold titletxt"> Welcome to your favorite tournament simulator</p>
+                        <p class="text-sm-start contenttxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            Repudiandae illo sint numquam
+                            perferendis quisquam praesentium magnam adipisci distinctio aliquid unde consequatur
+                            molestiae
+                            voluptate placeat, delectus velit iste inventore dolorum. Rem?</p>
+                    </div>
+                    <div class="container-fluid d-grid gap-2 d-md-flex justify-content-md-center">
+                        <a href="#" class="btn btn-success btn-lg">Get Started</a>
+                        <a href="#" class="btn btn-danger btn-lg">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
