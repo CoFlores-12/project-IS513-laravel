58ed4a7f1eaab0265ea4e5a64f2cebb2c7d9f2e1
josueamador19 <grodriguezd@unah.hn>
2023-11-30 17:43:15 -0600

    implements guardar persona

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -10,4 +10,26 @@ class PersonaController extends Controller
     public function crearPersona(){
         return view('agregarJugador');
     }
+    public function guardar(Request $request){
+        
+        $client = new Client(['base_uri' => 'http://localhost:8080/api/torneos/']);
+        $image = 'blank.png';
+        if ($request->hasFile('img')) {
+            $destinationPath = 'client';
+            $file = $request->file('img');
+            $filename = $file->getClientOriginalName();
+            $file->move($destinationPath, $filename);
+            $image = $filename;
+        }
+
+        $response = $client->request('POST', 'create', [
+            'json' => [
+                "nombre" => $request->name,
+                "apellido" => $request->apellido,
+                "foto" => $image
+
+            ]
+        ]);
+        return redirect()->route('torneos.home');
+    }
 }
